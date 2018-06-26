7fff98b29baee61c737e2a8f9b584bd162f35a0b
Johannes <keke5000@gmail.com>
2018-06-26 14:34:04 +0300

    Palauttaa Trains listan ja ottaa vastaan shortcodeja. Train.javassa muutettu Date LocalDateksi

--- src/main/java/vr/data/JsonReadData.java
+++ src/main/java/vr/data/JsonReadData.java
@@ -2,38 +2,23 @@ package vr.data;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
-
 import java.net.URI;
 import java.net.URL;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-/*
-Vaatii Jackson kirjaston:
-File | Project Structure
-Libraries >> Add >> Maven
-Etsi "jackson-databind", valitse esimerkiksi versio 2.0.5
-Asentuu Jacksonin databind, sekä core ja annotations
- */
 
 public class JsonReadData {
 
-    private String stations;
-
-    public void setStations(String departure){
-        //TODO String käsittelijä
+    private String setStations(String departure){
+        //TODO poistetaanko virheviesti?
         if(departure.isEmpty()){
             System.out.println("Error, station empty");
         }
         departure = departure.toUpperCase();
-        this.stations = departure;
+        return departure;
     }
 
-    public void setStations(String departure, String arrival){
-        //TODO String käsittelijä
+    private String setStations(String departure, String arrival){
+        //TODO poistetaanko virheviesti?
         if(departure.isEmpty()){
             System.out.println("Error, station empty");
         }
@@ -42,35 +27,26 @@ public class JsonReadData {
 
         String stationsCombined = departure + "/" + arrival;
 
-        this.stations = stationsCombined;
+        return stationsCombined;
 
     }
 
-    public void lueJunanJSONData() {
+    public ArrayList<Train> getTimeTable(String shortcodes) {
+        String stations = setStations(shortcodes);
         String baseurl = "https://rata.digitraffic.fi/api/v1";
+        ArrayList<Train> trains = new ArrayList<>();
+
         try {
             URL url = new URL(URI.create(String.format("%s/live-trains/station/" + stations, baseurl)).toASCIIString());
             ObjectMapper mapper = new ObjectMapper();
             CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Train.class);
-            List<Train> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
-
-            //TODO oma aliohjelmansa joka hakee ajat. Nyt vain testaus käyttö.
-            for (Train train : junat){
-
-                int junanNumero = train.getTrainNumber();
-                String junanType = train.getTrainType();
-                String junanCategory = train.getTrainCategory();
-
-                System.out.println("Junan tyyppi: " + junanType + "Junan numero: " + junanNumero + " Junan kategoria: " + junanCategory);
-
-                Date aika = train.getTimeTableRows().get(0).getScheduledTime();
-                LocalDate aikaLocalDate = LocalDate.ofInstant(aika.toInstant(), ZoneId.systemDefault());
-                System.out.println(aikaLocalDate);
-            }
+            trains = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
 
         } catch (Exception ex) {
             System.out.println(ex);
         }
+
+        return trains;
     }
 
 
