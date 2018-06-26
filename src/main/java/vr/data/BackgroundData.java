9e62c91d24b99c721ba9497bd6e6e41d49fb61c4
kaarinasuom <kaarina.suominen@outlook.com>
2018-06-26 15:27:36 +0300

    muokattu backgrounData luokkaa

--- src/main/java/vr/data/BackgroundData.java
+++ src/main/java/vr/data/BackgroundData.java
@@ -11,28 +11,56 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
+import vr.ui.UserInterface;
+
 public class BackgroundData {
 
-    public Map<String, String> generateShortCodes() {
+    private Map<String, String> stationShortCodes; //esitellään Hashmap
 
-        String baseurl="https://rata.digitraffic.fi/api/v1";
-        Map<String, String> map =new HashMap<> ();
+    public BackgroundData() { //parametriton konstruktoi
+
+        this.stationShortCodes = new HashMap<>(); //alustetaan hashmap
 
         try {
-            URL url=new URL (URI.create (String.format ("%s/metadata/stations", baseurl)).toASCIIString ()); // haettiin nämä URL datat, jotta saadaan
-            ObjectMapper mapper=new ObjectMapper ();
+            String baseurl="https://rata.digitraffic.fi/api/v1";
+            URL url=new URL (URI.create (String.format ("%s/metadata/stations", baseurl)).toASCIIString()); // haettiin nämä URL datat, jotta saadaan
+            ObjectMapper mapper=new ObjectMapper();
             CollectionType tarkempiListanTyyppi=mapper.getTypeFactory ().constructCollectionType (ArrayList.class, Stations.class);
             List<Stations> stations=mapper.readValue (url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
 
             for (Stations st : stations) {
-                map.put(st.getStationName (), st.getStationShortCode ());
+                this.stationShortCodes.put(st.getStationName(), st.getStationShortCode());
             }
 
         } catch (Exception ex) {
-            System.out.println (ex);
+            System.out.println(ex);
         }
 
-        return map;
+    }
+
+    public boolean isKey(String answer) { // method that tells the user if the map contains the key
+        return this.stationShortCodes.containsKey (answer);
+    }
+
+    public String getShortCode(String nameOfStation) { // method that should return the shortcode if found, null if not found
+        if (isKey (nameOfStation)) {
+            return this.stationShortCodes.get (nameOfStation);
+        } else {
+            return null;
+        }
+
+    }
+
+    public List<String> getNearestMatches(String match) {
+        // method that would return List of nearest String if similar String are found in Map
+        List<String> nearest = new ArrayList<>();  // luodaan lista
+        for (String name: stationShortCodes.keySet()){ // käy läpi jokaisen keysetin alkion
+            if (name.startsWith(match)) { // ottaa huomioon kaikki, jotka matchaavat kirjoitettuun alkuun
+                nearest.add(name); // lisää alkiot listaan
+            }
+        }
+        return nearest; // palauttaa listan lähimmistä asemista
     }
 
 }
+
