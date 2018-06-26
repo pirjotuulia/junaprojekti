e2dcdd5a2d38125d7baa19bdc8e94303a49e8169
pirjotuulia <pirjoleppanen@nic.fi>
2018-06-26 10:44:05 +0300

    ui ja app - ekat versiot

--- src/main/java/vr/App.java
+++ src/main/java/vr/App.java
@@ -1,13 +1,20 @@
 package vr;
 
+import vr.data.BackgroundData;
+import vr.ui.UserInterface;
+
+import java.util.Map;
+import java.util.Scanner;
+
 /**
- * Hello world!
- *
+ * by Pirjo
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        BackgroundData bgrdata = new BackgroundData();
+        Map<String, String> stationShortCodes = bgrdata.getStationShortCodes();
+        UserInterface ui = new UserInterface(reader, stationShortCodes);
+        ui.start();
     }
 }
--- src/main/java/vr/ui/UserInterface.java
+++ src/main/java/vr/ui/UserInterface.java
@@ -1,4 +1,139 @@
 package vr.ui;
 
+import vr.data.BackgroundData;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * by Pirjo
+ */
 public class UserInterface {
+    private Scanner reader;
+    private JsonRoadData trainData;
+    private Map<String, String> stationShortCodes;
+
+    public UserInterface(Scanner reader, Map<String, String> stationShortCodes) {
+        this.reader = reader;
+        this.stationShortCodes = stationShortCodes;
+    }
+
+    public void start() {
+        System.out.println("======================================");
+        System.out.println("          WE 'R HAPPINESS!  ");
+        System.out.println("======================================");
+        while (true) {
+            System.out.println("");
+            System.out.println(" 1) Timetable search - commuter trains");
+            System.out.println(" 2) Call Santa.");
+            System.out.println(" 3) Find nearest Sauna.");
+            System.out.println(" 4) Exit\n");
+            System.out.print("Your choice: ");
+            String answer = reader.nextLine();
+            if (answer.equals("1")) {
+                timetableSearch();
+            } else if (answer.equals("2")) {
+                System.out.println("Call Santa at Santa Claus Village amusement park, Rovaniemi: +358 16 3562096");
+            } else if (answer.equals("3")) {
+                System.out.println("Nearest sauna: Löyly at Hernesaarenranta 4, 00150 Helsinki");
+            } else if (answer.equals("4")) {
+                break;
+            }
+        }
+        System.out.println("Thank you for using our happiness search!");
+
+    }
+
+    private void timetableSearch() {
+        System.out.println("======================================");
+        System.out.println("     Travel by train in Finland.");
+        System.out.println("======================================");
+        System.out.println("No trains today!");
+        System.out.println("Just kidding.. ;-)");
+        System.out.println(" 1) Search for trains leaving from where you are now or any other station. If you just want to get away, no matter where you go!");
+        System.out.println(" 2) Search for trains going to a specific destination from where you are now. For the destination oriented!");
+        System.out.println(" 3) Is my train on time? Could be, but maybe you still have time to go for a cup of coffee and ice cream before it leaves?");
+        System.out.print(" Time to choose! ");
+        String answer = reader.nextLine();
+        if (answer.equals("1")) {
+            nextDepartures();
+        } else if (answer.equals("2")) {
+            timeTablesFromTo();
+        } else if (answer.equals("3")) {
+            trainLateOrInTime();
+        }
+    }
+
+    private void timeTablesFromTo() {
+        while (true) {
+            String departure = "";
+            String arrival = "";
+            System.out.print("Write the name of the station you wnat to depart from: ");
+            while (true) {
+                departure = reader.nextLine();
+                if (!this.stationShortCodes.containsKey(departure)) {
+                    System.out.print("The station you gave was not found on our system. Finnish spelling can be quite hard, please try again! ");
+                } else  {
+                    break;
+                }
+            }
+            System.out.print("Write the name of the station you want to go to: ");
+            while (true) {
+                arrival = reader.nextLine();
+                if (!this.stationShortCodes.containsKey(arrival)) {
+                    System.out.print("The station you gave was not found on our system. Finnish spelling can be quite hard, please try again! ");
+                } else {
+                    break;
+                }
+            }
+            String departureShortCode = this.stationShortCodes.get(departure);
+            String arrivalShortCode = this.stationShortCodes.get(arrival);
+            List<String> suitableTrains = trainData.getTimeTable(departureShortCode,arrivalShortCode);
+            if (!suitableTrains.isEmpty()) {
+                suitableTrains.stream().forEach(System.out::println);//ui presumes that all trains on the list are passenger trains.
+                break;
+            } else {
+                System.out.println("There are no connections from " + departure + " station to " + arrival + " station in the near future.");
+            }
+            System.out.print("Happy? Want to search for more departures? (y/n) ");
+            String answer = reader.nextLine();
+            if (answer.equals("n")) {
+                return;
+            }
+        }
+    }
+
+
+    private void nextDepartures() {
+        while (true) {
+            System.out.print("Write the name of the station you wnat to depart from: ");
+            while (true) {
+                String answer = reader.nextLine();
+                if (this.stationShortCodes.containsKey(answer)) {
+                    String stationShortCode = this.stationShortCodes.get(answer);
+                    List<String> suitableTrains = trainData.getTimeTable(stationShortCode);
+                    if (!suitableTrains.isEmpty()) {
+                        suitableTrains.stream().forEach(System.out::println);//ui presumes that all trains on the list are passenger trains.
+                        break;
+                    } else {
+                        System.out.println("There are no trains leaving from the " + answer + " station in the near future.");
+                    }
+                } else {
+                    System.out.print("The station you gave was not found on our system. Finnish spelling can be quite hard, please try again! ");
+                }
+            }
+            System.out.print("Happy? Want to search for more departures? (y/n) ");
+            String answer = reader.nextLine();
+            if (answer.equals("n")) {
+                return;
+            }
+        }
+    }
+
+    private void trainLateOrInTime() {
+        System.out.println("Sorry, we have no idea if your train's on time or not. Neither as VR..");
+    }
+
+
 }
