f063349b9503391889d2dafbd5c35029ef7cf120
kaarinasuom <kaarina.suominen@outlook.com>
2018-06-28 01:11:53 +0300

    kosmeettista kokeilua

--- src/main/java/vr/ui/UserInterface.java
+++ src/main/java/vr/ui/UserInterface.java
@@ -24,9 +24,15 @@ public class UserInterface {
     }
 
     public void start() {
-        System.out.println("========================================");
-        System.out.println("            WE'R HAPPINESS!  ");
-        System.out.println("========================================");
+        System.out.println ("              _                            _                       _        _                       _  ");
+        System.out.println ("__      _____| | ___ ___  _ __ ___   ___  | |_ ___   __      _____( )_ __  | |__   __ _ _ __  _ __ (_)_ __   ___  ___ ___");
+        System.out.println ("\\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\  \\ \\ /\\ / / _ \\/| '__| | '_ \\ / _` | '_ \\| '_ \\| | '_ \\ / _ \\/ __/ __|");
+        System.out.println (" \\ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |  \\ V  V /  __/ | |    | | | | (_| | |_) | |_) | | | | |  __/\\__ \\__ \\");
+        System.out.println ("  \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/    \\_/\\_/ \\___| |_|    |_| |_|\\__,_| .__/| .__/|_|_| |_|\\___||___/___/");
+        System.out.println ("                                                                                       |_|   |_|                          ");
+        System.out.println("=============================================");
+        System.out.println("            WE'R HAPPINESS AT YOUR SERVICE!  ");
+        System.out.println("=============================================");
         while (true) {
             System.out.println("");
             System.out.println(" 1) Timetable search - passenger trains");
