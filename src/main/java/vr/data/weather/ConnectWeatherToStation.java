61449a1bd4e82ec1c4a8394928321ad0257d7315
pirjotuulia <pirjoleppanen@nic.fi>
2018-06-28 17:37:58 +0300

    koordinaattien vertailu

--- src/main/java/vr/data/weather/ConnectWeatherToStation.java
+++ src/main/java/vr/data/weather/ConnectWeatherToStation.java
@@ -1,8 +1,30 @@
 package vr.data.weather;
 
-public class ConnectWeatherToStation {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 
-    public String connect() {
-        return "";
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectWeatherToStation {
+    private WeatherStationsJsonData jd;
+    private List<WeatherStation> stations;
+
+    public ConnectWeatherToStation() {
+        WeatherStationsJsonData jd = new WeatherStationsJsonData();
+        this.stations = jd.readWeatherStationJson();
+    }
+
+    public String findNearest(double longitude, double latitude) {
+        for (WeatherStation w : stations) {
+            Coord c = w.getCoord();
+            if (Math.abs(c.getLon()-longitude)<0.1&&Math.abs(c.getLat()-latitude)<0.1) {
+                return w.getName();
+            }
+        }
+        return null;
     }
 }
---
+++ src/main/java/vr/data/weather/WeatherStationsJsonData.java
@@ -0,0 +1,46 @@
+package vr.data.weather;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class WeatherStationsJsonData {
+    private List<WeatherStation> weatherStationsFI;
+
+    public List<WeatherStation> readWeatherStationJson() {
+        try {
+            File file = new File("weatherInFI.json");
+            ObjectMapper mapper = new ObjectMapper();
+            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, WeatherStation.class);
+            this.weatherStationsFI = mapper.readValue(file, tarkempiListanTyyppi);
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+//        Iterator<WeatherStation> it = weatherStations.iterator();
+//        while (it.hasNext()) {
+//            if (!it.next().getCountry().equals("FI")) {
+//                it.remove();
+//            }
+//        }
+        return weatherStationsFI;
+    }
+
+//    public void weatherStationsInFinlandToJson() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+//        try {
+//            objectMapper.writeValue(new File("weatherInFI.json"), this.weatherStations);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
